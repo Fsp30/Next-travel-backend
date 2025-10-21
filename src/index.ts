ae8b82582863ef45555f2ff453256b6f04072c57
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-21 20:16:13 -0300

    format code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express';
 
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
+const port = process.env.PORT;
 
-const port = process.env.PORT
+app.use(express.json());
 
-app.use(express.json())
-
-app.get("/", (req: Request, res: Response) => {
-        res.json({
-                message: "Next Travel Backend"
-        })
-})
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Next Travel Backend',
+  });
+});
 
 app.listen(port, () => {
-        console.log( `http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
