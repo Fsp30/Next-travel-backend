fe6f187270fa4f8fb4f882d637c553dac8db2a95
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-21 19:43:31 -0300

    test starting server

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, {Request, Response} from 'express'
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+
+const port = process.env.PORT
+
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+        res.json({
+                message: "Next Travel Backend"
+        })
+})
+
+app.listen(port, () => {
+        console.log( `http://localhost:${port}`)
+})
