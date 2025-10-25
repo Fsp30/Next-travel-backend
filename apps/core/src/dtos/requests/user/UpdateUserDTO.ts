035c24533799c427e4dbb1e339d84c5b45002c6f
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-25 20:58:56 -0300

    create CreateUserDTO dto/schema

--- apps/core/src/dtos/requests/user/UpdateUserDTO.ts
+++ apps/core/src/dtos/requests/user/UpdateUserDTO.ts
@@ -7,7 +7,7 @@ export const UpdateUserDTOSchema = z
     name: z
       .string()
       .min(2, 'O nome deve conter no minimo 2 caracteres')
-      .max(100, "O nome não deve ultrapassar 100 caracteres")
+      .max(100, 'O nome não deve ultrapassar 100 caracteres')
       .trim()
       .optional(),
 
