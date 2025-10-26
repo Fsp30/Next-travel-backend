eda47df86f674cbd31b80e984929facf697186ab
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-26 19:31:30 -0300

    create CostEstimateDTO  dto/schema

--- apps/core/src/dtos/responses/destination/CostEstimateDTO.ts
+++ apps/core/src/dtos/responses/destination/CostEstimateDTO.ts
@@ -1,48 +1,62 @@
-import z from "zod";
+import z from 'zod';
 
 export const TransportCostsDTOSchema = z.object({
-        bus: z.object({
-                min: z.number().optional(),
-                max: z.number().optional(),
-        }).optional(),
-        flight: z.object({
-                min: z.number().optional(),
-                max: z.number().optional(),
-        }).optional(),
-        currency: z.string().default('BRL'),
-})
+  bus: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+    })
+    .optional(),
+  flight: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+    })
+    .optional(),
+  currency: z.string().default('BRL'),
+});
 
 export const AccomodationCostsDTOSchema = z.object({
-        budget: z.object({
-                min: z.number().optional(),
-                max: z.number().optional(),
-        }).optional(),
-        midRange: z.object({
-                min: z.number().optional(),
-                max: z.number().optional(),
-        }).optional(),
-        luxury: z.object({
-                min: z.number().optional(),
-                max: z.number().optional(),
-        }).optional(),
-        currency: z.string().default('BRL')
-})
+  budget: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+    })
+    .optional(),
+  midRange: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+    })
+    .optional(),
+  luxury: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+    })
+    .optional(),
+  currency: z.string().default('BRL'),
+});
 
 export const CostEstimateDTOSchema = z.object({
-        transport: TransportCostsDTOSchema.optional(),
-        accomodation: AccomodationCostsDTOSchema.optional(),
-        estimateDailyBudget: z.object({
-                budget: z.number().optional(),
-                midRange: z.number().optional(),
-                luxury: z.number().optional(),
-        }).optional(),
-        totalEstimate: z.object({
-                min: z.number().optional(),
-                max: z.number().optional(),
-        }).optional(),
-        currency: z.string().default('BRL')
-})
+  transport: TransportCostsDTOSchema.optional(),
+  accomodation: AccomodationCostsDTOSchema.optional(),
+  estimateDailyBudget: z
+    .object({
+      budget: z.number().optional(),
+      midRange: z.number().optional(),
+      luxury: z.number().optional(),
+    })
+    .optional(),
+  totalEstimate: z
+    .object({
+      min: z.number().optional(),
+      max: z.number().optional(),
+    })
+    .optional(),
+  currency: z.string().default('BRL'),
+});
 
-export type TransportCostsDTO = z.infer<typeof TransportCostsDTOSchema>
-export type AccomodationCostsDTO = z.infer<typeof AccomodationCostsDTOSchema>
-export type CostEstimateDTO = z.infer<typeof CostEstimateDTOSchema>
+export type TransportCostsDTO = z.infer<typeof TransportCostsDTOSchema>;
+export type AccomodationCostsDTO = z.infer<typeof AccomodationCostsDTOSchema>;
+export type CostEstimateDTO = z.infer<typeof CostEstimateDTOSchema>;
