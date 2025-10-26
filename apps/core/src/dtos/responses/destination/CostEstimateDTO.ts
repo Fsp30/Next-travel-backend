4cc4bdfdafaba2556f878027c134d610435258d7
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-26 19:21:10 -0300

    create TransportCostsDTO dto/schema

---
+++ apps/core/src/dtos/responses/destination/CostEstimateDTO.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const TransportCostsDTOSchema = z.object({
+        bus: z.object({
+                min: z.number().optional(),
+                max: z.number().optional(),
+        }).optional(),
+        flight: z.object({
+                min: z.number().optional(),
+                max: z.number().optional(),
+        }).optional(),
+        currency: z.string().default('BRL'),
+})
+
+export type TransportCostsDTO = z.infer<typeof TransportCostsDTOSchema>
