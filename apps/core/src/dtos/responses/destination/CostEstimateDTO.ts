efc6c476c3feb3fde7e3230ac2395dd8420c410c
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-26 19:30:51 -0300

    create CostEstimateDTO dto/schema

--- apps/core/src/dtos/responses/destination/CostEstimateDTO.ts
+++ apps/core/src/dtos/responses/destination/CostEstimateDTO.ts
@@ -28,5 +28,21 @@ export const AccomodationCostsDTOSchema = z.object({
         currency: z.string().default('BRL')
 })
 
+export const CostEstimateDTOSchema = z.object({
+        transport: TransportCostsDTOSchema.optional(),
+        accomodation: AccomodationCostsDTOSchema.optional(),
+        estimateDailyBudget: z.object({
+                budget: z.number().optional(),
+                midRange: z.number().optional(),
+                luxury: z.number().optional(),
+        }).optional(),
+        totalEstimate: z.object({
+                min: z.number().optional(),
+                max: z.number().optional(),
+        }).optional(),
+        currency: z.string().default('BRL')
+})
+
 export type TransportCostsDTO = z.infer<typeof TransportCostsDTOSchema>
 export type AccomodationCostsDTO = z.infer<typeof AccomodationCostsDTOSchema>
+export type CostEstimateDTO = z.infer<typeof CostEstimateDTOSchema>
