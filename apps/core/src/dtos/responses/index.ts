456a305c654c4377ffbe7c4ce4d96d56b519348b
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-10-28 17:41:52 -0300

    export AutResponseDTO

--- apps/core/src/dtos/responses/index.ts
+++ apps/core/src/dtos/responses/index.ts
@@ -2,3 +2,4 @@ export * from './common';
 export * from './destination';
 export * from './search/SearchHistoryResponseDTO';
 export * from './user/UserResponseDTO';
+export * from './auth/AuthResponseDTO'
