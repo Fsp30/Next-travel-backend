838ddbea685887aafa35f1652f79b2c8c2e4206c
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-11-07 22:00:10 -0300

    export all use cases to cache

---
+++ apps/core/src/use-cases/cache/index.ts
@@ -0,0 +1,4 @@
+export * from './get-cached-response.use-case';
+export * from './save-cached-response.use-case';
+export * from './increment-cache-hit-count.use-case';
+export * from './delete-cache.use-case';
