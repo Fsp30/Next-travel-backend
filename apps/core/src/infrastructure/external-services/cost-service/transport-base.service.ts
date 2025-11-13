385244ac1c6457593923709d8e3956e0bb2dae8e
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-11-12 22:19:10 -0300

    add methods protected (calculateDistance and getDistanceFromCommonRoutes)

--- apps/core/src/infrastructure/external-services/cost-service/transport-base.service.ts
+++ apps/core/src/infrastructure/external-services/cost-service/transport-base.service.ts
@@ -1,3 +1,4 @@
+import { Coordinates } from '../../../domain/value-objects';
 import { BaseService } from '../../shared';
 import { AxiosClient } from '../http/axios.client';
 
@@ -26,5 +27,57 @@ export abstract class TransportBaseService<
       ...params,
       access_key: this.key,
     };
+  }
+  protected calculateDistance(
+    originCoordinates?: Coordinates,
+    destinationCoordinates?: Coordinates,
+    origin?: string,
+    destination?: string
+  ): number {
+    if (originCoordinates && destinationCoordinates)
+      return originCoordinates.distanceTo(destinationCoordinates);
+
+    if (origin && destination)
+      return this.getDistanceFromCommonRoutes(origin, destination);
+
+    return 1000; //fallback
+  }
+
+  protected getDistanceFromCommonRoutes(
+    origin: string,
+    destination: string
+  ): number {
+    const commonRoutes: Record<string, number> = {
+      'São Paulo-Rio de Janeiro': 360,
+      'Rio de Janeiro-São Paulo': 360,
+      'São Paulo-Brasília': 870,
+      'Brasília-São Paulo': 870,
+      'São Paulo-Salvador': 1450,
+      'Salvador-São Paulo': 1450,
+      'São Paulo-Fortaleza': 2370,
+      'Fortaleza-São Paulo': 2370,
+      'Rio de Janeiro-Salvador': 1214,
+      'Salvador-Rio de Janeiro': 1214,
+      'Rio de Janeiro-Brasília': 930,
+      'Brasília-Rio de Janeiro': 930,
+      'São Paulo-Belo Horizonte': 500,
+      'Belo Horizonte-São Paulo': 500,
+      'São Paulo-Curitiba': 330,
+      'Curitiba-São Paulo': 330,
+      'São Paulo-Porto Alegre': 860,
+      'Porto Alegre-São Paulo': 860,
+      'São Paulo-Florianópolis': 490,
+      'Florianópolis-São Paulo': 490,
+      'Rio de Janeiro-Belo Horizonte': 370,
+      'Belo Horizonte-Rio de Janeiro': 370,
+      'Juiz de Fora-São Paulo': 397,
+      'São Paulo-Juiz de Fora': 397,
+      'Juiz de Fora-Rio de Janeiro': 114,
+      'Rio de Janeiro-Juiz de Fora': 114,
+      'Juiz de Fora-Salvador': 1103,
+      'Salvador-Juiz de Fora': 1103,
+    };
+    const routeKey = `${origin}-${destination}`;
+    return commonRoutes[routeKey] || 1000;
   }
 }
