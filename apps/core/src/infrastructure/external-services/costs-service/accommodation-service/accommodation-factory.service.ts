be1e83ea89b26c724750c19c8d0dc28780730378
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-11-19 19:37:26 -0300

    create Accommodation Service Factory

--- apps/core/src/infrastructure/external-services/costs-service/accommodation-service/accommodation-factory.service.ts
+++ apps/core/src/infrastructure/external-services/costs-service/accommodation-service/accommodation-factory.service.ts
@@ -1,5 +1,24 @@
-import { AccommodiationOrchestratorService } from "./accommodation-orchestrator.service";
+import { CalculateAccommodationCostsService } from './accommodation-calculate-costs.service';
+import { AccommodationOrchestratorService } from './accommodation-orchestrator.service';
+import { ListHotelsService } from './accomodation-list-hotels.service';
 
-export class AccommodationServiceFactory{
-        static create(): AccommodiationOrchestratorService
+export class AccommodationServiceFactory {
+  static create(): AccommodationOrchestratorService {
+    const listHotelsService = new ListHotelsService();
+    const calculateCostsService = new CalculateAccommodationCostsService();
+
+    return new AccommodationOrchestratorService(
+      listHotelsService,
+      calculateCostsService
+    );
+  }
+  static createWithServices(
+    listHotelsService: ListHotelsService,
+    calculateCostsService: CalculateAccommodationCostsService
+  ): AccommodationOrchestratorService {
+    return new AccommodationOrchestratorService(
+      listHotelsService,
+      calculateCostsService
+    );
+  }
 }
