55d53d592cb16dab89699fcceb3ee5b925915387
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-11-24 21:07:54 -0300

    export orchestrator and factory weather

--- apps/core/src/infrastructure/external-services/weather-service/index.ts
+++ apps/core/src/infrastructure/external-services/weather-service/index.ts
@@ -2,3 +2,5 @@ export * from './weather-base.service';
 export * from './weather-current.service';
 export * from './weather-forecast.service';
 export * from './weather-seasonal.service';
+export * from './weather-orchestrator.service';
+export * from './weather-factory.service';
