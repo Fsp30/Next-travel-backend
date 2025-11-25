24590b22eb6f563622ef8777cd787bc9545b1f9c
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-11-24 21:01:27 -0300

    export orchestrator and factory

--- apps/core/src/infrastructure/external-services/wikipedia-service/index.ts
+++ apps/core/src/infrastructure/external-services/wikipedia-service/index.ts
@@ -2,3 +2,5 @@ export * from './wikipedia-base.service';
 export * from './wikipedia-city-info.service';
 export * from './wikipedia-search.service';
 export * from './wikipedia-summary.service';
+export * from './wikipedia-orchestrator.service';
+export * from './wikipedia-factory.service';
