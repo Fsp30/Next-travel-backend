c5f9fddd170aaa80c24a3f2c6f2a3e042c8c040f
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-12-21 21:20:55 -0300

    fix: correction params and clean unnecessary methods

--- apps/core/src/interfaces/repositories/ICityRepository.ts
+++ apps/core/src/interfaces/repositories/ICityRepository.ts
@@ -1,22 +1,20 @@
 import { City } from '../../domain/entities/City';
 import { CityId, Coordinates } from '../../domain/value-objects';
 
-export interface CreateCityData {
-  name: string;
-  state: string;
-  country?: string;
-  coordinates?: Coordinates;
+export interface ICitiesQueriesProps {
+  skip?: number;
+  take?: number;
+  where?: {
+    state: string;
+    country?: string;
+  };
+  orderBy?: {
+    createdAt?: 'asc' | 'desc';
+  };
 }
 
-export interface FindCittiesOptions {
-  country?: string;
-  state?: string;
-  isPopular?: boolean;
-  limit?: number;
-  offset?: number;
-}
 export interface ICityRepository {
-  create(data: CreateCityData): Promise<City>;
+  create(data: City): Promise<City>;
 
   findById(id: string | CityId): Promise<City | null>;
   findBySlug(slug: string): Promise<City | null>;
@@ -28,22 +26,15 @@ export interface ICityRepository {
 
   update(city: City): Promise<City>;
   incrementCount(id: string | CityId): Promise<void>;
-  upateCoordinates(
+  updateCoordinates(
     id: string | CityId,
     coordinates: Coordinates
   ): Promise<void>;
 
-  findPopularCitties(limit?: number): Promise<City[]>;
-  findMany(options: FindCittiesOptions): Promise<City[]>;
-  findOrCreate(data: CreateCityData): Promise<City>;
-  findTopSearchCitties(limit?: number): Promise<
-    Array<{
-      city: City;
-      requestCount: number;
-    }>
-  >;
+  findPopularCitties(limit?: number): Promise<City[] | null>;
+  findMany(options: ICitiesQueriesProps): Promise<City[] | null>;
 
   delete(city: City): Promise<void>;
 
-  count(options?: FindCittiesOptions): Promise<number>;
+  count(): Promise<number>;
 }
