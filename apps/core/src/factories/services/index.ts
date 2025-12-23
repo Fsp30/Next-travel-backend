9f7217a551f9f74907fb843d17a3fc8b3e916676
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-12-23 20:07:27 -0300

    att barrell export

--- apps/core/src/factories/services/index.ts
+++ apps/core/src/factories/services/index.ts
@@ -1,3 +1,5 @@
 export * from './costs-factory.service';
 export * from './wikipedia-factory.service';
 export * from './weather-factory.service';
+export * from './llm.factory';
+export * from './auth.factory';
