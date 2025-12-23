cb103f548033dd5cebfafc16efc3bdd905b9f68b
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-12-23 18:13:11 -0300

    barrell export

---
+++ apps/core/src/factories/repositories/index.ts
@@ -0,0 +1,4 @@
+export * from './cache-repository.factory';
+export * from './user-repository.factory';
+export * from './city-repository.factory';
+export * from './search-history-repository.factory';
