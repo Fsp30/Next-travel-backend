0ce46a4f481aec38a6e006b0d02106fca65caa99
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-12-28 20:10:07 -0300

    fix: update FastifyInstace

--- apps/core/src/presentation/types/fastify.d.ts
+++ apps/core/src/presentation/types/fastify.d.ts
@@ -1,11 +1,12 @@
 import { AppFactory } from '../../factories';
 declare module 'fastify' {
+  type AppInstance = ReturnType<typeof AppFactory.getInstance>;
+
   interface FastifyRequest {
     user?: {
       id: string;
       email: string;
     };
+    app: AppInstance;
   }
-
-  ReturnType<typeof AppFactory.getInstance>;
 }
