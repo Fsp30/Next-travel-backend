6d581497d6ea675b116dd3dde1fe3b0ae86e8eec
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2025-12-29 21:56:36 -0300

    correction name

--- apps/core/src/factories/use-cases/user/update-user.factory.ts
+++ apps/core/src/factories/use-cases/user/update-user.factory.ts
@@ -1,7 +1,7 @@
 import { IUserRepository } from '@/core/src/interfaces';
 import { UpdateUserUseCase } from '@/core/src/use-cases/user';
 
-export class UdateUserFactory {
+export class UpdateUserFactory {
   static create(userRepository: IUserRepository): UpdateUserUseCase {
     return new UpdateUserUseCase(userRepository);
   }
