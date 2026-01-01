97f241cab45f4c8c49f9a533d6f09c8e2a3a3a96
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-01-01 19:35:01 -0300

    define contract FastifySchema

---
+++ apps/core/src/presentation/http/contracts/types/schema.d.ts
@@ -0,0 +1,11 @@
+export interface FastifySchema {
+  description?: unknown;
+  tags?: unknown[];
+  body?: unknown;
+  summary?: unknown;
+  querystring?: unknown;
+  security?: unknown;
+  params?: unknown;
+  headers?: unknown;
+  response?: unknown;
+}
