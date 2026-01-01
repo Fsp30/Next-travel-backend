80fd6864c250de14b9387989af8b82768e3219f7
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-01-01 20:30:14 -0300

    create user routes

---
+++ apps/core/src/presentation/http/routes/user.routes.ts
@@ -0,0 +1,53 @@
+import { FastifyInstance } from 'fastify';
+import { UserController } from '../controllers';
+import { UpdateUserDTOSchema } from '@/core/src/dtos';
+import { authMiddleware, validationMiddleware } from '../../middlewares';
+import {
+  deleteUserAccountSchema,
+  getUserHistorySchema,
+  getUserProfileSchema,
+  updateUserProfileSchema,
+} from '../schemas';
+
+export async function userRoutes(fastify: FastifyInstance) {
+  const controller = new UserController();
+
+  fastify.get(
+    '/me',
+    {
+      schema: getUserProfileSchema,
+      preHandler: [authMiddleware],
+    },
+    controller.getProfile.bind(controller)
+  );
+
+  fastify.put(
+    '/me',
+    {
+      schema: updateUserProfileSchema,
+      preHandler: [
+        authMiddleware,
+        validationMiddleware(UpdateUserDTOSchema, 'body'),
+      ],
+    },
+    controller.updateProfile.bind(controller)
+  );
+
+  fastify.delete(
+    '/me',
+    {
+      schema: deleteUserAccountSchema,
+      preHandler: [authMiddleware],
+    },
+    controller.deleteAccount.bind(controller)
+  );
+
+  fastify.get(
+    '/me/history',
+    {
+      schema: getUserHistorySchema,
+      preHandler: [authMiddleware],
+    },
+    controller.getSearchHistory.bind(controller)
+  );
+}
